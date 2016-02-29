99812142fa06c7465a6db3d131af29e994a8f7f3
jlvivero <josluivivgar@gmail.com>
2016-02-29 02:09:25 -0800

    fixed a bunch of bugs, basic functionality is working

--- calculator.cpp
+++ calculator.cpp
@@ -2,12 +2,17 @@
 #include "ui_calculator.h"
 #include "parser.h"
 #include <QRegExp>
+#include <iostream>
+#include <string>
 
 Calculator::Calculator(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+    //idk if anything i put after this is supposed to be here
+    QObject::connect(ui->lineEdit, SIGNAL(returnPressed()), this, SLOT(onReturnPressed()));
+    ui->plainTextEdit->setReadOnly(true);
 }
 
 Calculator::~Calculator()
@@ -21,6 +26,7 @@ void Calculator::on_lineEdit_returnPressed()
     QString answer;
     std::vector<QString> process;
     long long ans;
+    ui->plainTextEdit->setPlainText("");
     ui->textBrowser->setText("");
 
     //TODO: change the regular expression so that the string is only changed to string + =, do this after initial testing
@@ -31,8 +37,8 @@ void Calculator::on_lineEdit_returnPressed()
         {
             answer = answer + process[i] + "\n";
         }
-        answer = answer + ans;
         ui->textBrowser->setText(answer);
+        ui->plainTextEdit->setPlainText(std::to_string(ans).c_str());
     }
     else
     {
--- parser.cpp
+++ parser.cpp
@@ -12,12 +12,14 @@ long long convert(std::vector<QString> & process, const QString & s)
     long long val1;
     long long val2;
     bool negative = false;
+    bool first = true;
     std::stack<long long> valueStack;
     std::stack<std::string> opStack;
     std::string str = s.toStdString();
     std::string token;
     std::string pastToken;
     int j = 0;
+    str.pop_back();
 
     while(!str.empty())
     {
@@ -26,36 +28,45 @@ long long convert(std::vector<QString> & process, const QString & s)
         //this assumes that the equation is already validated as having only 12 digits per number, and that the sequence of +/*- etc, are not together
         //TODO: make a cpp that has all the possible tokens with regex instead of this solution, do this after initial testings
         j = int (str.find_first_of("+-/*()"));
-        if(j == -1) //if the last value is an int
-        {
-            token = str;
-            value = std::stoll(token);
-            valueStack.push(value);
-            str.erase(0);
-            continue;
-        }
         if(j == 0) // if it starts with a negative number
         {
             negative = true;
             str.erase(0,1);
-            continue;
+            j = int (str.find_first_of("+-/*()"));
         }
 
+        if(j == -1)
+        {
+            token = str;
+        }
+        else
+        {
         token = str.substr(0,j);
+        }
+
         value = std::stoll(token,nullptr);
-        if(negative)
+
+        if(negative && first)
         {
             value = value * -1;
             negative = false;
+            first = false;
         }
+        first = false;
         valueStack.push(value);
+        if(j == -1)
+        {
+            str.erase(0);
+        }
+        else
+        {
         str.erase(0,j);
+        }
         //up to here you get the number and add it to the value
 
         token = str[0];
         //this is just to make sure the switch works fine with a character
         thisOP = token[0];
-
         //TODO: this might not be needed if i can detect what kind of token it is  with the complimentary cpp i need to do
         switch(thisOP)
         {
@@ -88,11 +99,11 @@ long long convert(std::vector<QString> & process, const QString & s)
                 opStack.push(token);
                 break;
             case '*':
-                while(opStack.size() != 0 && !opStack.top.compare('+') && !opStack.top.compare('-'))
+                while(opStack.size() != 0 && !opStack.top().compare("+") && !opStack.top().compare("-"))
                 {
                     pastToken = opStack.top();
                     opStack.pop();
-                    pastOP = pastToken[0]
+                    pastOP = pastToken[0];
                     val1 = valueStack.top();
                     valueStack.pop();
                     val2 = valueStack.top();
@@ -102,7 +113,7 @@ long long convert(std::vector<QString> & process, const QString & s)
                 opStack.push(token);
                 break;
             case '/':
-                while(opStack.size() != 0 && !opStack.top.compare('+') && !opStack.top.compare('-'))
+                while(opStack.size() != 0 && !opStack.top().compare("+") && !opStack.top().compare("-"))
                 {
                     pastToken = opStack.top();
                     opStack.pop();
@@ -119,7 +130,7 @@ long long convert(std::vector<QString> & process, const QString & s)
                 opStack.push(token);
                 break;
             case ')':
-                while(opStack.top != '(')
+                while(!opStack.top().compare("("))
                 {
                     pastToken = opStack.top();
                     opStack.pop();
@@ -131,14 +142,16 @@ long long convert(std::vector<QString> & process, const QString & s)
                     valueStack.push(evaluate(val1,val2,pastOP,process));
                 }
                 opStack.pop();
-            default: //special case
+                break;
+            default: break;//special case
         }
-
     }//TODO: use regex properly to check if the string is a number
-    while(opStack.size != 0)
+
+    while(opStack.size() != 0)
     {
-        char thisOP
-        token = opStack.pop();
+        char thisOP;
+        token = opStack.top();
+        opStack.pop();
         thisOP = token[0];
         val1 = valueStack.top();
         valueStack.pop();
@@ -146,12 +159,13 @@ long long convert(std::vector<QString> & process, const QString & s)
         valueStack.pop();
         valueStack.push(evaluate(val1,val2,thisOP,process));
     }
-    value = valueStack.pop();
+    value = valueStack.top();
+    valueStack.pop();
     return value;
 }
 
 
-long long evaluate(long long value1, long long value2, const char & op, std::vector<Qstring> & process )
+long long evaluate(long long value1, long long value2, const char & op, std::vector<QString> & process )
 {
     //TODO: validate that the operations are viable
     QString str;
@@ -164,7 +178,7 @@ long long evaluate(long long value1, long long value2, const char & op, std::vec
         case '-':
             str = value1 + "-" + value2;
             process.push_back(str);
-            return value1 - value2;
+            return value2 - value1;
         case '*':
             str = value1 + "*" + value2;
             process.push_back(str);
@@ -172,6 +186,7 @@ long long evaluate(long long value1, long long value2, const char & op, std::vec
         case '/':
             str = value1 + "/" + value2;
             process.push_back(str);
-            return value1 / value2;
+            return value2 / value1;
     }
+    return 0;
 }

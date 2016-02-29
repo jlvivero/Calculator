dea2d115e459051457837aa0f7923e68c3e844a9
jlvivero <josluivivgar@gmail.com>
2016-02-28 22:16:38 -0800

    fixed something on the regex

--- calculator.cpp
+++ calculator.cpp
@@ -24,7 +24,7 @@ void Calculator::on_lineEdit_returnPressed()
     ui->textBrowser->setText("");
 
     //TODO: change the regular expression so that the string is only changed to string + =, do this after initial testing
-    if(s.contains(QRegExp("^\d{1,12}((\+|-|\*|\/)\d{1,12})*\=|d{1,12}\=|-\d{1,12}((\+|-|\*|\/)\d{1,12})*\=")))
+    if(s.contains(QRegExp("^\\d{1,12}((\\+|-|\\*|\\/)\\d{1,12})*\\=|d{1,12}\\=|-\\d{1,12}((\\+|-|\\*|\\/)\\d{1,12})*\\=")))
     {
         ans = convert(process,s);
         for(int i = 0; i < process.size(); i++)
